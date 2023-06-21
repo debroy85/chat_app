f3464a362ab069583c8b3ab9fa757db763f0238c
sugata06 <119696077+sugata06@users.noreply.github.com>
2023-06-21 15:36:41 +0530

    Add files via upload

---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatPage = () => {
+  return (
+    <div>
+      This is CHAT PAGE
+    </div>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      This is HOME PAGE
+    </div>
+  )
+}
+
+export default HomePage
